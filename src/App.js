aa9cf9ac0227bd5f353c15a6794ace385be2aad2
Andrew Smith <quarsol@yahoo.com>
2018-06-19 15:27:44 -0600

    Header and Footer components working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+// import Profiles from "./components/Profiles"
+// import SkillList from "./components/SkillList"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Header />
+        <Footer />
       </div>
     );
   }
